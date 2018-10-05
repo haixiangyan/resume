81e98f48d45b841077729672e67e71f326d3ceaf
Haixiang6123 <haixiang6123@gmail.com>
2018-10-04 19:56:36 -0700

    Final 1.0

--- src/assets/info.js
+++ src/assets/info.js
@@ -47,7 +47,7 @@ export default {
         techs: [
             'Vue.js',
             'React.js',
-            'Test utils (Karma, Sinon, Chai, Travis CI, Mocha)',
+            'Test tools (Mocha, Sinon, Chai, Karma, Travis CI)',
             'Node.js',
             'Android'
         ],
@@ -55,14 +55,15 @@ export default {
     },
     projects: [
         {
-            left: 'Wheel UI',
+            left: 'Wheel UI Widgets Library',
             link: 'https://haixiang6123.github.io/wheel-ui/',
             right: 'July 2018',
             children: [
                 {left: 'Developed a UI widgets library with *Vue.js*, such as Grid system, Toast, Carousel, Cascader, Tabs and etc.'},
-                {left: 'Utilized a CSS pre-processor *SCSS* to write widgets styles, and encapsulated some Mixins for reusing styles'},
-                {left: 'Wrote unit tests with assertion and stubbing frameworks like *Chai*, *Sinon* and *Mocha*. Combined *Travis CI* and *Karma* to do integrating testing'},
-                {left: 'Set up documentation website with *vuepress* and deployed it on github'}
+                {left: 'Utilized a CSS pre-processor *SCSS* to write widgets styles and encapsulated some Mixins for reusing styles'},
+                {left: 'Wrote unit tests with assertion and stubbing frameworks like *Chai*, *Sinon* and *Mocha*'},
+                {left: 'Combined *Travis CI* and *Karma* to do integration testing'},
+                {left: 'Set up documentation website with *VuePress* and deployed it on github'}
             ]
         },
         {
@@ -70,11 +71,10 @@ export default {
             link: 'https://haixiang6123.github.io/guitar-editor/#/',
             right: 'Mar 2018',
             children: [
-                {left: 'Implemented a guitar tabs editor with *React.js* and *svg* technology'},
-                {left: 'Inspired by markdown and laTex, it renders formatted plain texts to React components to display a guitar tab'},
+                {left: 'Implemented a guitar tabs editor with *React.js* and *SVG* technology'},
+                {left: 'Inspired by markdown and LaTex, it renders formatted plain texts to React components to display a guitar tab'},
                 {left: 'Managed edit page and preview page with *React Router*, so that guitar tabs data can be shared to both pages'},
                 {left: 'Assigned text mode and chord mode for showing chords with plain texts or chord svgs above each lyrics'},
-                {left: 'Published *Chord* component and *Lyrics* Component to npm library'}
             ]
         },
         {
@@ -83,15 +83,15 @@ export default {
             right: 'Feb 2018',
             children: [
                 {left: 'Used *jQuery*, *HTML5* and *CSS3* to draw a Pikachu on a web page'},
-                {left: 'Made it to draw a Pikachu while writing CSS codes dynamically'}
+                {left: 'Made it to draw a Pikachu dynamically while writing CSS codes'}
             ]
         },
         {
-            left: 'Movie gallery system',
+            left: 'Movie Gallery System',
             right: 'Jan 2018',
             children: [
-                {left: 'The movie gallery system is built with *React.js*'},
-                {left: 'Controlled movie data, search results and user information with *Redux*'},
+                {left: 'Built the movie gallery system with *React.js* for users to view, search and purchase moives'},
+                {left: 'Controlled movie data, search results and shopping carts with *Redux*'},
                 {left: 'Set up a router system with user authentication and route guards using *React Router*'},
                 {left: 'Utilized Google *reCAPTCHA* to prevent from robot attacks'},
                 {left: 'Tested system performance with *Tomcat apache JMeter*'},
@@ -101,15 +101,15 @@ export default {
     ],
     interns: [
         {
-            left: 'Neusoft',
+            left: 'NEUSoft',
             link: 'https://haixiang6123.github.io/yan-xuan/#/home/recommend',
             right: 'May 2017 - Sept 2017',
             children: [
-                {left: 'Front-end development'},
+                {left: 'Frontend development'},
                 {left: 'Developed basic UI widgets and web pages for an online-shopping web app (Mobile) with *Vue.js*'},
                 {left: 'Used *Vue Router* to build the router system and organized global data with *Vuex*'},
-                {left: 'Wrote test codes with *Mock.js* to test response data of back end APIs'},
-                {left: 'Helped to implement shopping cart module which includes adding default address, modifying cart items and developed a Cascader widget'},
+                {left: 'Wrote test codes with *Mock.js* to test response data of backend APIs'},
+                {left: 'Helped to implement shopping cart module which includes adding default address, modifying cart items and developing an address Cascader widget'},
             ]
         }
     ]
