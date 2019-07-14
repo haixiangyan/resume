f2d3ee369626407da2060ff518b40b55b51624cd
Haixiang6123 <haixiang6123@gmail.com>
2019-07-14 16:27:23 -0700

    Update - 2019.7

--- src/assets/info.js
+++ src/assets/info.js
@@ -1,8 +1,8 @@
 export default {
     name: 'Haixiang Yan',
-    tel: '+19493467358',
+    tel: '+1(949)346-7358',
     email: 'haixiany@uci.edu',
-    linkedin: 'https://haixiang6123.github.io/blog/#/',
+    website: 'https://haixiang6123.github.io/blog/#/',
 	github: 'https://github.com/Haixiang6123',
     edus: [
         {
@@ -10,8 +10,8 @@ export default {
             right: 'Irvine, CA',
             children: [
                 {
-                    left: 'M.S. Networked System',
-                    right: 'Jun 2020'
+                    left: 'M.S. Networked System (GPA: 3.3/4)',
+                    right: 'Jun 2019'
                 }
             ]
         },
@@ -20,47 +20,43 @@ export default {
             right: 'Shenyang, China',
             children: [
                 {
-                    left: 'B.S. Engineering in Software Engineering (GPA: 86/100)',
+                    left: 'B.S. Engineering in Software Engineering (GPA: 3.67/5)',
                     right: 'Jun 2018'
                 },
                 {
-                    left: 'Honors: Third-class Scholarship (15%, Northeastern University)',
+                    left: 'Honors: Third-class Scholarship (Top 15%)',
                 },
-                {
-                    left: 'Study abroad - UC Irvine (2017 - 2018)'
-                }
             ]
         },
     ],
     skills: {
-        langs: ['JavaScript', 'Java', 'python'],
-        frontend: ['React.js', 'Vue.js', 'jQuery', 'ES6', 'Sass', 'CSS3'],
-        test: ['Mocha', 'Sinon', 'Chai', 'Karma', 'Travis CI', 'JMeter'],
-        mobile: ['React Native', 'Android'],
+        langs: ['JavaScript', 'Java', 'Python'],
+        frontend: ['React.js', 'Vue.js', 'Angular', 'TypeScript', 'ES6', 'Sass', 'GraphQL', 'Restful API'],
+        test: ['Mocha', 'Sinon', 'Chai', 'Karma', 'Jest', 'Puppteer', 'Travis CI'],
+        others: ['React Native', 'Android'],
     },
     projects: [
+        {
+            left: 'Overwatch UI Library',
+            link: 'https://haixiang6123.github.io/overwatch-ui-doc/#/',
+            right: 'Mar 2019',
+            children: [
+                {left: 'A mobile-first, Overwatch-themed UI library built with *Vue.js* (10K downloads, 160+ stars)'},
+                {left: 'Modularized components styles with *Sass*, and used it to encapsulate Mixins and Functions'},
+                {left: 'Used *Chai*, *Sinon* and *Mocha* to write unit tests for each component and configured *Travis CI* environment'},
+                {left: 'Component implementations are based on features provided by Vue.js, such as dependency injection, transition, slots and etc.'}
+            ]
+        },
         {
             left: 'Code Block Beautifier',
             link: 'https://github.com/Haixiang6123/codeblock-beautifier',
             right: 'Jan 2019',
             children: [
-                {left: 'Built a *Chrome Extension* to highlight codes in any website has "pre" element'},
-                {left: 'Provides over 20 languages for auto-detection and over 70 different themes'},
-                {left: 'Applied DFS algorithm to combine continuous "pre" elements as one "pre" element'},
-                {left: 'Strong compatible: Supporting Medium, StackOverflow, MDN, Jianshu and Zhihu'},
-                {left: 'Used Observer Pattern to dispatch and trigger events of changing theme and language preference'}
-            ]
-        },
-        {
-            left: 'Overwatch UI Library',
-            link: 'https://haixiang6123.github.io/overwatch-ui-doc/#/',
-            right: 'July 2018',
-            children: [
-                {left: 'Developed a UI widgets library with *Vue.js* and published on npm to let developers beatify their website'},
-                {left: 'Upgraded component styles codes by using *Sass* to encapsulate some Mixins for reusing styles'},
-                {left: 'Wrote unit tests with assertion and stubbing frameworks like Chai, Sinon and Mocha'},
-                {left: 'Improved robustness by combining Travis CI and Karma to do integration testing'},
-                {left: 'Deployed official documentation website with VuePress to demonstrate details of APIs for developers'},
+                {left: 'A *Chrome Extension* for adding code syntax highlighting to any technical articles (400+ users, 90+ stars).'},
+                {left: 'Designed a DFS algorithm to combine continuous "pre" elements as one "pre" element'},
+                {left: 'Provides more than 20 languages for auto-detection and over 70 different themes'},
+                {left: 'Applied "Observer" design pattern to dispatch and trigger built-in events, such as changing theme and switching language'},
+                {left: 'Strong compatible: For now, it supports Medium, StackOverflow, MDN, FreeCodeCamp, etc.'},
             ]
         },
         {
@@ -68,34 +64,34 @@ export default {
             link: 'https://haixiang6123.github.io/guitar-editor/#/',
             right: 'Mar 2018',
             children: [
-                {left: 'Designed a guitar tabs editor with *React.js* for users to develop guitar tabs by writing formatted texts'},
-                {left: "Rendered Markdown/LaTex texts to a guitar tab to reduce user's learning costs"},
-                {left: 'Formatted guitar tabs size by using *SVG* to display guitar chords'},
-                {left: 'Implemented 2 chord displaying modes so that user can choose to show chords above lyrics in text or SVG'},
-                {left: 'Published component Chord to let developers customize their own chords and use standard chords with React.js'}
-            ]
-        },
-        {
-            left: 'Movie Gallery System',
-            right: 'Jan 2018',
-            children: [
-                {left: 'Built the movie gallery system with React.js for users to view, search and purchase movies'},
-                {left: 'Improved website security by setting route guards with React Router and detecting robot with Google reCAPTCHA'},
-                {left: 'Controlled movie data, search results and shopping carts with *Redux* and shared them to common components'},
-                {left: 'Accessing same APIs developed with Servlet, a Android app was built to make it more flexible and scalable'},
+                {left: 'An online gutar tabs editor built with *React.js*'},
+                {left: "Rendered formatted texts to a guitar tab, and used *SVG* to display guitar symbols"},
+                {left: 'Implemented 2 modes to display guitar tabs: Display chord as text or a SVG element'},
+                {left: 'Open source a React component for enabling developers to customize guitar chords'},
             ]
         },
     ],
     interns: [
         {
+            left: 'DJI Technology Company (Title: Frontend Developer)',
+            right: 'Jun 2019 - Sept 2019',
+            children: [
+                {left: 'Designed and implemented *LAANC system* which helps drone pilots to make flight requests'},
+                {left: 'Restyled components based on new design, and migrated their implementations to *Element UI* from *Buefy + Bulma*'},
+                {left: "Implemented Map module requirements with *Mapbox*, such as selecting flight area, analyzing restricted area and fetching local weathers"},
+                {left: 'Designed schemas, queries and mutations of *GraphQL* and used *Vue-Apollo* to send Ajax requests'},
+                {left: 'Used *express.js* and *mockserver* to generate mock data to do integration tests with *Puppteer*'},
+            ]
+        },
+        {
             left: 'Neusoft (Title: Frontend Developer)',
             right: 'May 2017 - Sept 2017',
             children: [
-                {left: 'Collaborated teams to develop UI components for an online shopping web app (Mobile) with Vue.js and evaluated the designs of components with UI designers'},
-                {left: 'Reduced 48% component codes by introducing Vuex to manage data flow between components'},
-                {left: "Developed a privilege management system by using Vue Router to authorize different level privileges to users"},
-                {left: 'Beautified and rebuilt Vue components with Animate.css and Element UI'},
-                {left: 'Automated backend APIs and HTTP requests testing by writing scripts with Mock.js'},
+                {left: 'Developed UI components for an *online shopping web app* (Mobile) with Vue.js'},
+                {left: 'Optimized redundant codes by using *Vuex* to managing data flow among unrelated components'},
+                {left: 'Configured *Vue Router guard* to manage user accessibility based on privileges'},
+                {left: 'Added transition and beautified components with Animate.css and Element UI'},
+                {left: 'Used *Mock.js* to generate dummy data and test all HTTP requests'}
             ]
         }
     ]
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -6,14 +6,11 @@
         <li>
             <strong>Frontend: </strong>{{skills.frontend.join(', ')}}
         </li>
-        <!--<li>-->
-            <!--<strong>Backend: </strong>{{skills.backend.join(', ')}}-->
-        <!--</li>-->
-        <li>
-            <strong>Mobile: </strong>{{skills.mobile.join(', ')}}
-        </li>
         <li>
             <strong>Test: </strong>{{skills.test.join(', ')}}
+        </li>
+        <li>
+            <strong>Others: </strong>{{skills.others.join(', ')}}
         </li>
     </ul>
 </template>
